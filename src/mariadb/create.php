ffaf9bdda1d37cc207f31829f3ecd5652a03dd36
Gaspard Vandecasteele <gaspard.vandecasteele+fixe@gmail.com>
2023-09-16 22:59:07 +0200

    fix the create script

--- src/mariadb/create.php
+++ src/mariadb/create.php
@@ -12,6 +12,7 @@ $port = Environment::getEnv("MARIADB_PORT");
 
 $conn = new PDO("mysql:host=$server;dbname=$database;port=$port", $user, $password);
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$conn->setAttribute(PDO::ATTR_AUTOCOMMIT, FALSE);
 
 $sql = file_get_contents("schema.sql");
 
@@ -21,15 +22,16 @@ $secure_password = password_hash($clear_password, PASSWORD_DEFAULT);
 
 try {
     $conn->beginTransaction();
-    $conn->exec($sql);
+    $conn->query($sql);
     $sql = "INSERT INTO `user` (`username`, `password`, `balance`) 
             VALUES ('user_clear_password', 'my_password', '123456.50'), 
                    ('user_md5_password', '$md5_password', '654321.99'), 
-                   ('user_secure_password, '$secure_password', '456789.00')";
+                   ('user_secure_password', '$secure_password', '456789.00')";
+    $conn->query($sql);
     $conn->commit();
     echo "Table and data have been created";
 } catch (PDOException $e) {
     $message = $e->getMessage();
     $conn->rollBack();
-    echo "Error : $message";
+    die($message);
 }
